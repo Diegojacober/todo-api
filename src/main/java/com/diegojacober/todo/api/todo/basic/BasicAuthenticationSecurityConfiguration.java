ba8286ebf5413a9c9cee260cbcbb4bd52cbb90c5
Diego Alencar Jacober <57761933898>
2023-09-20 14:52:31 -0300

    Allow all Options requests

--- src/main/java/com/diegojacober/todo/api/todo/basic/BasicAuthenticationSecurityConfiguration.java
+++ src/main/java/com/diegojacober/todo/api/todo/basic/BasicAuthenticationSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.diegojacober.todo.api.todo.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,7 +17,9 @@ public class BasicAuthenticationSecurityConfiguration {
     // stateless rest api
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated()).httpBasic(Customizer.withDefaults())
+        //.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+        
+        return http.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest().authenticated()).httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).csrf()
                 .disable()
                 .build();
