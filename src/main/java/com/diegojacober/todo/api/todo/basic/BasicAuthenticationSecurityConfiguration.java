dfe0f2b7263c8ac72ba57c0b0436bad9d124a2d3
Diego Alencar Jacober <57761933898>
2023-09-20 16:53:39 -0300

    Using mysql

--- src/main/java/com/diegojacober/todo/api/todo/basic/BasicAuthenticationSecurityConfiguration.java
+++ src/main/java/com/diegojacober/todo/api/todo/basic/BasicAuthenticationSecurityConfiguration.java
@@ -1,7 +1,6 @@
 package com.diegojacober.todo.api.todo.basic;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
